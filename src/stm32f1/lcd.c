bc7fbfd3a6e29a476d97bbdf576d8d921fff441d
Jimmy Wennlund <jimmy@wennlund.nu>
2018-03-31 01:49:04 +0200

    try to get hw i2c working

--- src/stm32f1/lcd.c
+++ src/stm32f1/lcd.c
@@ -12,18 +12,104 @@
 #include "u8g2.h"
 #include "adc.h"
 #include "stringutil.h"
+#include "debug.h"
 
 static u8g2_t u8g2;
 static volatile int ready = 0;
+
+static uint8_t u8x8_byte_stm32_hw_spi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  uint8_t *data;
+  uint32_t reg32 __attribute__((unused));
+
+ 
+  switch(msg)
+  {
+    case U8X8_MSG_BYTE_SEND:
+      data = (uint8_t *)arg_ptr;
+      debug("hw_i2c send: ");
+      debug_put_int8(arg_int);
+      debug(" bytes\n");
+
+      while( arg_int > 0 )
+      {
+        i2c_send_data(I2C2, *data);
+        while (!(I2C_SR1(I2C2) & I2C_SR1_BTF)) {
+            if (I2C_SR1(I2C2) & I2C_SR1_BTF)
+                debug("BTF\n");
+            if (I2C_SR1(I2C2) & I2C_SR1_TxE)
+                debug("TxE\n");
+            if (I2C_SR1(I2C2) & I2C_SR1_ADDR)
+                debug("ADDR\n");
+            if (I2C_SR1(I2C2) & I2C_SR1_SB)
+                debug("SB\n");
+
+        }
+        //while (!(I2C_SR1(I2C2) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+        data++;
+        arg_int--;
+      }
+      break;
+    case U8X8_MSG_BYTE_INIT:
+      debug("hw_i2c init\n");
+      rcc_periph_clock_enable(RCC_GPIOB);
+      rcc_periph_clock_enable(RCC_I2C2);
+      rcc_periph_clock_enable(RCC_AFIO);
+      gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+		      GPIO_I2C2_SCL | GPIO_I2C2_SDA);
+      i2c_peripheral_disable(I2C2);
+      i2c_set_clock_frequency(I2C2, I2C_CR2_FREQ_42MHZ);
+      //i2c_set_fast_mode(I2C2);
+      i2c_set_standard_mode(I2C2);
+      //i2c_set_ccr(I2C2, 0x1e);
+      //i2c_set_trise(I2C2, 0x0b);
+      i2c_set_ccr(I2C2, 35);
+      i2c_set_trise(I2C2,43);
+      i2c_set_own_7bit_slave_address(I2C2, 0x00);
+      i2c_peripheral_enable(I2C2);
+      break;
+      
+    case U8X8_MSG_BYTE_SET_DC:
+      break;
+      
+    case U8X8_MSG_BYTE_START_TRANSFER: {
+      uint8_t addr = u8x8_GetI2CAddress(u8x8);
+      debug("hw_i2c start addr: ");
+      debug_put_hex8(addr);
+      debug("\n");
+      i2c_send_start(I2C2);
+      while (!((I2C_SR1(I2C2) & I2C_SR1_SB)
+		& (I2C_SR2(I2C2) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+      
+      i2c_send_7bit_address(I2C2, addr, I2C_WRITE);
+      while (!(I2C_SR1(I2C2) & I2C_SR1_ADDR));
+      reg32 = I2C_SR2(I2C2);
+      break;
+    }
+    case U8X8_MSG_BYTE_END_TRANSFER:
+      debug("hw_i2c end\n");
+      i2c_send_stop(I2C2);
+      break;
+    default:
+      return 0;
+  }
+  return 1;
+}
+
+static uint8_t u8x8_gpio_and_delay_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
+    return 1;
+}
+
 
 static uint8_t u8x8_gpio_and_delay_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
   switch(msg)
   {
     case U8X8_MSG_GPIO_AND_DELAY_INIT:
+      debug("sw_i2c init\n");
       gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO10);
       gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
-      // Init 
       break;
     case U8X8_MSG_DELAY_NANO:
       /* not required for SW I2C */
@@ -77,7 +163,8 @@ static uint8_t u8x8_gpio_and_delay_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_in
 }
 
 void lcd_init(void) {
-  u8g2_Setup_ssd1306_i2c_128x64_noname_2(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_i2c);
+  u8g2_Setup_ssd1306_i2c_128x64_noname_2(&u8g2, U8G2_R0, u8x8_byte_stm32_hw_spi, u8x8_gpio_and_delay_hw_i2c);
+  //u8g2_Setup_ssd1306_i2c_128x64_noname_2(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_i2c);  
 
   u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
   u8g2_SetPowerSave(&u8g2, 0); // wake up display
